8719976dd342f1000980df208002c3a515f1dc03
jjk12 <jjk12@DESKTOP-AT8Q130>
2020-04-21 00:28:39 +0900

    메소드 이용까지 추가

--- src/TimesTable.java
+++ src/TimesTable.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class TimesTable {
 	public static void main(String[] args) {
 		//2,3단 계산 출력
-		
 		System.out.println("2단");
 		System.out.println(2*1);
 		System.out.println(2*2);
@@ -14,6 +13,7 @@ public class TimesTable {
 		System.out.println(2*7);
 		System.out.println(2*8);
 		System.out.println(2*9);
+		
 		System.out.println("3단");
 		System.out.println(3*1);
 		System.out.println(3*2);
@@ -52,7 +52,7 @@ public class TimesTable {
 		
 		System.out.println("5단");
 		int whnum=1;
-		while(whnum<=9) {
+		while(whnum<10) {
 			result = 5*whnum;
 			whnum++;
 			System.out.println(result);
@@ -61,7 +61,7 @@ public class TimesTable {
 		//6단 반복문 for 사용 
 		
 		System.out.println("6단");
-		for(int i=1;i<=9;i++) {
+		for(int i=1;i<10;i++) {
 			result = 6*i;
 			System.out.println(result);
 		}
@@ -72,15 +72,55 @@ public class TimesTable {
 		System.out.println("몇 단을 출력하시겠습니까?");
 		Scanner scanner = new Scanner(System.in);
 		int scannernum = scanner.nextInt();
-		System.out.println(scannernum);
+		System.out.println(scannernum + "단을 출력합니다");
 		for(int i= 1;i<=9;i++) {
 			System.out.println(scannernum*i);
 		}
 		
-		//
-			
+		// 조건 문을 사용하여 오루 검출
+		System.out.println("몇 단을 출력하시겠습니까?");
+		scannernum = scanner.nextInt();
+		if(2<=scannernum&&scannernum<=9) {
+			for(int i=1;i<10;i++)
+				System.out.println(scannernum*i);
+		}
+		else {
+			System.out.println("2에서 9사이의 숫자를 입력해주십시오");
+		}
 		
+		//배열을 이용
 		
+		int[] arr = new int [9];
+		for(int i=2;i<10;i++) {
+			for(int j=0;j<arr.length;j++) {
+				arr[j]=i*(j+1);
+			}
+			for(int k=0;k<arr.length;k++) {
+				System.out.println(arr[k]);
+			}
+		}
+		
+		// 메소드를 이용해보자
+		int[] arr2 = new int [9];
+		for(int i=2;i<10;i++){
+			System.out.println("메소드 입니다.");
+			arr2 = calc(i);
+			print(arr2);
+		}
+	}
+	//계산 메소드이다
+	public static int[] calc(int a) {
+		int [] arr  = new int[9];
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = a*(i+1);
+		}
+		return arr;	
+	}
+	//출력 메소드이다
+	public static void print(int[] arr2) {
+		for(int i=0;i<arr2.length;i++) {
+			System.out.println(arr2[i]);
+		}
 	}
 }
 
